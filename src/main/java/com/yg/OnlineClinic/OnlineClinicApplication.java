a7eed23b9f6104ca5acca8bb65c8efed5e139c86
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-12 17:31:17 +0530

    first commit

---
+++ src/main/java/com/yg/OnlineClinic/OnlineClinicApplication.java
@@ -0,0 +1,13 @@
+package com.yg.OnlineClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yg/OnlineClinic/OnlineClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.OnlineClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
