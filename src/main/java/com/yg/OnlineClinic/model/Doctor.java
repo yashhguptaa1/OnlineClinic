3a9327e715a30f89940c0cbe241a0ea1e626fcd0
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-13 15:43:57 +0530

    adding POJOs. Closes #2

---
+++ src/main/java/com/yg/OnlineClinic/model/Doctor.java
@@ -0,0 +1,4 @@
+package com.yg.OnlineClinic.model;
+
+public class Doctor extends Person{
+}
---
+++ src/main/java/com/yg/OnlineClinic/model/Pet.java
@@ -0,0 +1,34 @@
+package com.yg.OnlineClinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Guardian guardian;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Guardian getGuardian() {
+        return guardian;
+    }
+
+    public void setGuardian(Guardian guardian) {
+        this.guardian = guardian;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
