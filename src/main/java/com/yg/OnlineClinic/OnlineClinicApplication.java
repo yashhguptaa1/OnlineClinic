976cbd2794d3712425deea42fe23c7218bf14242
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-13 10:29:25 +0530

    notes imgs updated 1

--- src/main/java/com/yg/OnlineClinic/OnlineClinicApplication.java
+++ src/main/java/com/yg/OnlineClinic/OnlineClinicApplication.java
@@ -1,13 +1,27 @@
 package com.yg.OnlineClinic;
 
+import com.yg.OnlineClinic.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class OnlineClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineClinicApplication.class, args);
+		/*
+		press command and hover mouse over .run
+		public static ConfigurableApplicationContext run(Class<?> primarySource, String... args) {
+        return run(new Class[]{primarySource}, args);
+    }
+		 */
+
+		ApplicationContext ctx=SpringApplication.run(OnlineClinicApplication.class, args);
+		MyController myController=(MyController)ctx.getBean("myController");
+
+		String greeting=myController.Hello();
+
+		System.out.println(greeting);
+
 	}
-// checking
 }
--- src/main/java/com/yg/OnlineClinic/controllers/MyController.java
+++ src/main/java/com/yg/OnlineClinic/controllers/MyController.java
@@ -1,4 +1,16 @@
 package com.yg.OnlineClinic.controllers;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+
+    public String Hello()
+    {
+        System.out.print("Hello ");
+
+        return "yashgupta";
+
+    }
 }
