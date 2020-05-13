a0711edb12c1155279e96e68f90ecd8502e750d5
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-13 18:06:34 +0530

    adding base entity. closes #9

--- online-clinic-data/src/main/java/com/yg/OnlineClinic/model/PetType.java
+++ online-clinic-data/src/main/java/com/yg/OnlineClinic/model/PetType.java
@@ -1,4 +1,14 @@
 package com.yg.OnlineClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
