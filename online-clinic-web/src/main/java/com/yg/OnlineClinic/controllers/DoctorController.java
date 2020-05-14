07fe5508754a446624f264902f1b8437801d9396
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-14 13:35:49 +0530

    added doctors list and updated controller. closes #14

--- online-clinic-web/src/main/java/com/yg/OnlineClinic/controllers/DoctorController.java
+++ online-clinic-web/src/main/java/com/yg/OnlineClinic/controllers/DoctorController.java
@@ -1,14 +1,25 @@
 package com.yg.OnlineClinic.controllers;
 
+import com.yg.OnlineClinic.services.DoctorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DoctorController {
 
+    private final DoctorService doctorService;
+
+    // using spring to autowire automatically
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+
     @RequestMapping({"/doctors","/doctors/index","/doctors/index.html"})
-    public String listDoctors()
+    public String listDoctors(Model model)
     {
+        model.addAttribute("doctors",doctorService.findAll());
         return "doctors/index";
     }
 }
