81da4d31f9ef2e28b1e559e6d2d5844fe59fdbed
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-14 21:03:05 +0530

    added visits,updated guardians, REMOVED ERRORS IN POM.closes #24

--- online-clinic-data/src/main/java/com/yg/OnlineClinic/model/Guardian.java
+++ online-clinic-data/src/main/java/com/yg/OnlineClinic/model/Guardian.java
@@ -1,4 +1,16 @@
 package com.yg.OnlineClinic.model;
 
+import java.util.Set;
+
 public class Guardian extends Person{
+
+    private Set<Pet>pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
