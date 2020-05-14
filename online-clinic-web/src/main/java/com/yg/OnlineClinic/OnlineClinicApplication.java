822aa950cf4084ce105e899031c88955c5a50033
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-14 07:18:51 +0530

    Added Online Clinic Index Page and Controller. closes #7

--- online-clinic-web/src/main/java/com/yg/OnlineClinic/OnlineClinicApplication.java
+++ online-clinic-web/src/main/java/com/yg/OnlineClinic/OnlineClinicApplication.java
@@ -2,7 +2,6 @@ package com.yg.OnlineClinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class OnlineClinicApplication {
@@ -15,7 +14,7 @@ public class OnlineClinicApplication {
     }
 		 */
 
-		ApplicationContext ctx=SpringApplication.run(OnlineClinicApplication.class, args);
+		SpringApplication.run(OnlineClinicApplication.class, args);
 
 
 
