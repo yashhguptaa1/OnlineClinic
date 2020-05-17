fd6d5575225d439ae3fdcd97a1bbcf338b8b5637
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-18 01:51:28 +0530

    added specialities,updated doctor.closes #25

--- online-clinic-data/src/main/java/com/yg/OnlineClinic/model/Doctor.java
+++ online-clinic-data/src/main/java/com/yg/OnlineClinic/model/Doctor.java
@@ -1,4 +1,16 @@
 package com.yg.OnlineClinic.model;
 
+import java.util.Set;
+
 public class Doctor extends Person{
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
---
+++ online-clinic-data/src/main/java/com/yg/OnlineClinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.yg.OnlineClinic.model;
+
+public class Speciality extends BaseEntity {
+
+    public String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
