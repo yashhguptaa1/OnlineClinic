a9120cd1cb2d25a8368a27e6356fa95eb57a72ce
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-18 10:02:59 +0530

    added petTypes in DataLoader .closes #42

--- online-clinic-web/src/main/java/com/yg/OnlineClinic/bootstrap/DataLoader.java
+++ online-clinic-web/src/main/java/com/yg/OnlineClinic/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package com.yg.OnlineClinic.bootstrap;
 
 import com.yg.OnlineClinic.model.Doctor;
 import com.yg.OnlineClinic.model.Guardian;
+import com.yg.OnlineClinic.model.PetType;
 import com.yg.OnlineClinic.services.DoctorService;
 import com.yg.OnlineClinic.services.GuardianService;
+import com.yg.OnlineClinic.services.PetTypeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,19 +15,31 @@ public class DataLoader implements CommandLineRunner {
 
     private final GuardianService guardianService;
     private final DoctorService doctorService;
+    private final PetTypeService petTypeService;
 
     // dont need to use @autowire as we used spring to create this constructor
-    public DataLoader(GuardianService guardianService, DoctorService doctorService)
+    public DataLoader(GuardianService guardianService, DoctorService doctorService,PetTypeService petTypeService)
     {
 
 
         this.guardianService = guardianService;
         this.doctorService = doctorService;
+        this.petTypeService=petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType cough=new PetType();
+        cough.setName("cough");
+        PetType savedCoughType=petTypeService.save(cough);
+
+        PetType cold=new PetType();
+        cough.setName("cold");
+        PetType savedColdType=petTypeService.save(cold);
+
+
 
         Guardian guardian1=new Guardian();
         guardian1.setFirstName("Sunit"); //from extending Person
