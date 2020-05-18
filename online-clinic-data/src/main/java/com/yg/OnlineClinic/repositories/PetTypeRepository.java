566734781ab5f1def293af3108596f7fbc8d8212
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-18 20:50:51 +0530

    Added Spring data jpa services for PetType.closes #39

--- online-clinic-data/src/main/java/com/yg/OnlineClinic/repositories/PetTypeRepository.java
+++ online-clinic-data/src/main/java/com/yg/OnlineClinic/repositories/PetTypeRepository.java
@@ -1,4 +1,7 @@
 package com.yg.OnlineClinic.repositories;
 
-public interface PetTypeRepository {
+import com.yg.OnlineClinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeRepository extends CrudRepository<PetType,Long> {
 }
