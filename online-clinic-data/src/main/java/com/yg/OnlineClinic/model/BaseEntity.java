3b9d98678ab649f309390e7c3e349c6c743afc5f
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-18 18:39:04 +0530

    Implemented JPA annotations in Base Entity.closes #32

--- online-clinic-data/src/main/java/com/yg/OnlineClinic/model/BaseEntity.java
+++ online-clinic-data/src/main/java/com/yg/OnlineClinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package com.yg.OnlineClinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// we dont need this specific class mapped to DB
+// other classes will be inheriting from this class
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
